36a1aac9b634807abb4e8f89173fac8e2ebf4ea0
Derejawu Baye <derejawubaye09@gmail.com>
2026-01-19 23:18:03 +0300

    Add NotificationProvider to context providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,13 @@ import BaseUrlContextProvider from './contexts/BaseUrlContext.jsx'
 import CartContextProvider from './contexts/CartContext.jsx'
 import SubcategoryIdProvider from './contexts/SubcategoryIdContext.jsx'
 import QuantitiesContextProvider from './contexts/QuantitiesContext.jsx'
+import { NotificationProvider } from './contexts/NotificationContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   
     <BrowserRouter>
+
+    <NotificationProvider>
     
       <BaseUrlContextProvider>
      <CartContextProvider>
@@ -21,6 +24,6 @@ createRoot(document.getElementById('root')).render(
     </SubcategoryIdProvider>
     </CartContextProvider> 
     </BaseUrlContextProvider>
-      
+      </NotificationProvider>
     </BrowserRouter>
 )
