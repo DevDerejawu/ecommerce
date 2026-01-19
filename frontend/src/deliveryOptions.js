665c7118669d94ccca91e5bd42fe95a5c437267e
Derejawu Baye <derejawubaye09@gmail.com>
2026-01-20 01:23:25 +0300

    Create deliveryOptions.js

---
+++ frontend/src/deliveryOptions.js
@@ -0,0 +1,6 @@
+export const deliveryOptions = [
+  { price: 20, range: "Within 3 days" },
+  { price: 15, range: "Within 5 days" },
+  { price: 10, range: "Within 7 days" },
+  { price: 0, range: "After 7 days" },
+];
