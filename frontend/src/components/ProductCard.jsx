76050492fc5b67c3c8e3a9a89e200723ed8855fa
Derejawu Baye <derejawubaye09@gmail.com>
2026-01-20 03:11:14 +0300

    Update ProductCard.jsx

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
+import { UsePopUpMessage } from "../contexts/NotificationContext";
 import { useContext, useState } from "react";
 import { Star } from "lucide-react";
 import axios from "axios";
-import { seQuantitiesContext } from "../contexts/QuantitiesContext";
-import { UsePopUpMessage } from "../contexts/NotificationContext";
+import { useQuantitiesContext } from "../contexts/QuantitiesContext";
 import { BaseUrlContext } from "../contexts/BaseUrlContext";
 import { cartContext } from "../contexts/CartContext";
 function ProductCard({ product }) {
@@ -12,8 +12,7 @@ function ProductCard({ product }) {
   const { baseUrl } = useContext(BaseUrlContext);
   const { getQty, increaseQty, decreaseQty } = useQuantitiesContext();
   const [lodingPosting, setLoadingPosting] = useState(false);
-
-
+  
   async function handleAddToCart(product) {
     const price = product.price;
     const productId = product.id;
@@ -31,19 +30,24 @@ function ProductCard({ product }) {
       );
 
       if (res.data.success) {
-        showPopUpMessage(res.data.message);
+        showPopUpMessage(res.data.message)
+        
       } else {
-        showPopUpMessage(res.data.message, "error");
+        showPopUpMessage("Unable to add to cart.", "error")
+       
       }
     } catch (error) {
-    if(error.response){
-    showPopUpMessage(error.response.data.message, "error");
-  }else{
-    showPopUpMessage("Unknown error", "error");
-    } 
-  }finally {
+      if(error.response){
+        showPopUpMessage(error.response.data.message);
+      }else{
+        showPopUpMessage("Unable to add to cart.", "error")
+        
+      }
+    } finally {
       setLoadingPosting(false);
+      
     }
+  }
 
   return (
     <div className="group relative cursor-pointer border rounded-lg overflow-hidden bg-white pb-3">
@@ -64,7 +68,7 @@ function ProductCard({ product }) {
 
         <Link
           to={`/detail-page/${product.id}`}
-          className="absolute cursor-pointer bottom-0 w-full text-center  bg-black border px-3 py-3 text-sm rounded opacity-0 text-white group-hover:opacity-100 transition"
+          className="absolute cursor-pointer bottom-0 w-full text-center bg-black border px-3 py-3 text-sm opacity-100 rounded md:opacity-0 text-white md:group-hover:opacity-100 transition"
         >
           View Detail
         </Link>
@@ -79,7 +83,8 @@ function ProductCard({ product }) {
         }}
         disabled={lodingPosting}
       >
-        {lodingPosting?<Spinner />: "Add to Cart"}
+        {lodingPosting?<Spinner/>: "Add to Cart"}
+
       </button>
 
       {/* Product info */}
@@ -87,9 +92,7 @@ function ProductCard({ product }) {
         <h3 className="font-semibold">{product.name}</h3>
         <p className="text-gray-600">${product.price}</p>
 
-         <p className="text-center text-[19px] text-green-400">
-        {lodingPosting ? lodingPosting : message}
-      </p>
+         
         <div className="flex flex-row gap-[8px] justify-center items-center mt-[6px]">
           <button
             className="border px-2 py-1 cursor-pointer rounded disabled:bg-gray-400 disabled:cursor-not-allowed"
